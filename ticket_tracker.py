c3149a09f6a1a4a929b17de757b1af580641b9c8
tylersnel <snelgrove86@gmail.com>
2022-09-28 14:46:00 -0600

    Finished the input portion of a ticket and added verifications for correctt inputs

--- ticket_tracker.py
+++ ticket_tracker.py
@@ -1,6 +1,6 @@
 import sqlite3
 from datetime import datetime
-from actiontracker import Ticket
+from ticket_class import Ticket
 conn = sqlite3.connect(':memory:') #'ticket.db'
 
 c = conn.cursor()
@@ -14,6 +14,7 @@ c.execute("""CREATE TABLE ticket (
     type text
     )""")
 
+############# Input Validators #############
 def date_validator(date_text):
     try:
         if date_text != datetime.strptime(date_text, "%Y-%m-%d").strftime('%Y-%m-%d'):
@@ -22,33 +23,56 @@ def date_validator(date_text):
     except ValueError:
         return False
 
+def action_validator():
+    n=-1
+    while True:
+        if n<0:
+            act_type = input('Please enter type of action. Pay, admin or iPERMS: ')
+            act_type = act_type.lower()
+            if act_type == 'pay' or act_type == 'admin' or act_type == 'iperms':
+                return act_type
+            else:
+                n+=1
+        else:
+            act_type = input('Input must be Pay, admin or iPERMS only. Please try again: ')
+            act_type = act_type.lower()
+            if act_type == 'pay' or act_type == 'admin' or act_type == 'iperms':
+                return act_type
+
+############# SQL #############
 def insert_ticket(new_ticket):
     with conn: #no longer need of commit statment
         c.execute("INSERT INTO ticket VALUES (:first, :last, :rank, :date, :unit, :type)" ,
               {'first':new_ticket.first, 'last':new_ticket.last, 'rank':new_ticket.rank, 'date':new_ticket.date, 'unit':new_ticket.unit, 'type':new_ticket.type })
+
+def delete_ticket(ticket):
+    with conn:
+        c.execute()
 
 
 inp= input("Here to add a ticker? Y/N: ")
 inp=inp.lower()
 
 if inp == 'yes' or inp=='y':
-    # f_name=input('Please enter First Name: ')
-    # l_name = input('Please enter Last Name: ')
-    #
-    # rank = input('Please enter rank, 3 letter format only: ')# add verifier
-    # while len(rank)!=3:
-    #     rank = input('Please enter rank, 3 letter format only: ')
+    f_name=input('Please enter First Name: ')
+    l_name = input('Please enter Last Name: ')
+
+    rank = input('Please enter rank, 3 letter format only: ')
+    while len(rank)!=3:# verify length. 3 letter format only
+        rank = input('Incorrect format. Please enter rank, 3 letter format only: ')
 
     date = input('Please enter date (YYYY-MM-DD): ')
     if not date_validator(date):
-        date = input('Please enter date (YYYY-MM-DD): ')
+        date = input('Incorrect format. Please enter date (YYYY-MM-DD): ')
         date_validator(date)
 
 
     unit = input('Please enter unit: ')
-    act_typ = input('Please enter type of action: ')
 
-    sm=Ticket(f_name, l_name, rank, date, unit, act_typ)
+    act_type=action_validator()
+
+
+    sm=Ticket(f_name, l_name, rank, date, unit, act_type)
     insert_ticket(sm)
 
 else:
