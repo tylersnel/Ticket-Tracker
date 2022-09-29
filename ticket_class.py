9f52ea81d66759f89a3a41ded41070b750ae6270
tylersnel <snelgrove86@gmail.com>
2022-09-29 08:51:17 -0600

    changed type to act_type in class

--- ticket_class.py
+++ ticket_class.py
@@ -1,12 +1,12 @@
 class Ticket:
 
-    def __init__(self, first,last, rank, date, unit, type):
+    def __init__(self, first,last, rank, date, unit, act_type):
         self.first = first
         self.last = last
         self.rank = rank
         self.date = date
         self.unit = unit
-        self.type = type
+        self.act_type = act_type
 
     def first(self):
         return self.first
@@ -23,5 +23,5 @@ class Ticket:
     def unit(self):
         return self.unit
 
-    def type(self):
-        return self.type
+    def act_type(self):
+        return self.act_type
