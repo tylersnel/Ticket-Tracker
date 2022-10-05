c25f23d6b30f6d7ba91a80fd2ab5fb0039f9a331
tylersnel <snelgrove86@gmail.com>
2022-10-05 09:02:35 -0600

    Added TL to table. Working on update. Get rid of some inputs?

--- ticket_tracker.py
+++ ticket_tracker.py
@@ -8,14 +8,15 @@ c = conn.cursor()
 
 ############# Table Creator #############
 #if table doesn't exist already in DB, un-comment
-# c.execute("""CREATE TABLE tickets (
-#     first text,
-#     last text,
-#     rank text,
-#     date text,
-#     unit integer,
-#     act_type text
-#     )""")
+c.execute("""CREATE TABLE tickets (
+     first text,
+     last text,
+     rank text,
+     date text,
+     unit integer,
+     act_type text,
+     tl_num text
+     )""")
 
 ############# Input Validators #############
 def date_validator(date_text):
@@ -48,9 +49,9 @@ def action_validator():
 def insert_ticket(new_ticket):
     #Inserts ticket into ticket table based off inputs from calling fuction
     with conn: #no longer need of commit statment
-        c.execute("INSERT INTO tickets VALUES (:first, :last, :rank, :date, :unit, :act_type)" ,
+        c.execute("INSERT INTO tickets VALUES (:first, :last, :rank, :date, :unit, :act_type, :tl_num)" ,
               {'first':new_ticket.first, 'last':new_ticket.last, 'rank':new_ticket.rank, 'date':new_ticket.date,
-               'unit':new_ticket.unit, 'act_type':new_ticket.act_type })
+               'unit':new_ticket.unit, 'act_type':new_ticket.act_type, 'tl_num' :new_ticket.tl_num })
 
 def delete_ticket(first,last, rank,date, unit, act_type):
     #Deletes ticket using SQL based on inputs from calling function
@@ -59,58 +60,25 @@ def delete_ticket(first,last, rank,date, unit, act_type):
                   {'first': first, 'last': last, 'rank': rank, 'date': date,
                    'unit': unit, 'act_type': act_type} )
 
+def update_ticket(first, last, date, act_type):
+    with conn:
+        c.execute(
+            "UPDATE tickets WHERE first=:first AND last =:last AND date =:date AND act_type= :act_type",
+            {'first': first, 'last': last, 'date': date, 'act_type': act_type})
 
-def get_insert_inputs():
-    #Gets input from the user, then sends inputs to the insert function
-    f_name=input('Please enter First Name: ')
-    l_name = input('Please enter Last Name: ')
-
-    rank = input('Please enter rank, 3 letter format only: ')
-    while len(rank)!=3:# verify length. 3 letter format only
-        rank = input('Incorrect format. Please enter rank, 3 letter format only: ')
-
-    date = input('Please enter date (YYYY-MM-DD): ')
-    if not date_validator(date):
-        date = input('Incorrect format. Please enter date (YYYY-MM-DD): ')
-        date_validator(date)
-
-
-    unit = input('Please enter unit: ')
-
-    act_type=action_validator()
-
-
-    sm=Ticket(f_name, l_name, rank, date, unit, act_type)
-    insert_ticket(sm)
-
-def get_delete_inputs():
-    #Gets inputs for the user and then sends those inputs to the delete function
-    print("!WARNING! THIS WILL DELETE TICKET BASED ON INFO GIVEN")
-    time.sleep(3)
-    f_name = input('Please enter First Name: ')
-    l_name = input('Please enter Last Name: ')
-
-    rank = input('Please enter rank, 3 letter format only: ')
-    while len(rank) != 3:  # verify length. 3 letter format only
-        rank = input('Incorrect format. Please enter rank, 3 letter format only: ')
-
-    date = input('Please enter date (YYYY-MM-DD): ')
-    if not date_validator(date):
-        date = input('Incorrect format. Please enter date (YYYY-MM-DD): ')
-        date_validator(date)
-
-    unit = input('Please enter unit: ')
-
-    act_type = action_validator()
-
-    delete_ticket(f_name, l_name, rank, date, unit, act_type)
 
 def get_totals(outer_loop):
+    # function that prints out total actions completed by type.
+    # recieves a False variable for outer loop. If command not recognized,
+    # loop starts again. If recognized, loop ends and prints totals.
+    # returns nothing.
     while not outer_loop:
-        inp=input('Totals by unit, action type or all?: ')
+        inp=input('Totals by unit, action type or all? Type return to go back: ')
         inp=inp.lower()
         with conn:
-            if inp=='all':
+            if inp=='return':
+                outer_loop=True
+            elif inp=='all':
                 c.execute("SELECT COUNT(*) FROM tickets")
                 print(str(c.fetchall()[0][0]) + ' total actions completed') # to get to the element in the list tuple
                 outer_loop=True
@@ -136,6 +104,67 @@ def get_totals(outer_loop):
 
             else:
                 print('Action type not recognized, please try again.')
+
+def get_insert_inputs():
+    #Gets input from the user, then sends inputs to the insert function
+    f_name=input('Please enter First Name: ')
+
+    l_name = input('Please enter Last Name: ')
+
+    rank = input('Please enter rank, 3 letter format only: ')
+    while len(rank)!=3:# verify length. 3 letter format only
+        rank = input('Incorrect format. Please enter rank, 3 letter format only: ')
+    rank=rank.lower()
+
+    date = input('Please enter date (YYYY-MM-DD): ')
+    if not date_validator(date):
+        date = input('Incorrect format. Please enter date (YYYY-MM-DD): ')
+        date_validator(date)
+
+
+    unit = input('Please enter unit: ')
+
+    act_type=action_validator()
+
+    if act_type=='pay':
+        loop_val=False
+        while not loop_val:
+            tl_num=input('Enter TL number if you have it, press enter if you do not: ')
+            if tl_num.isdigit() or not tl_num:
+                Ticket(f_name, l_name, rank, date, unit, act_type, tl_num)
+                sm = Ticket(f_name, l_name, rank, date, unit, act_type, tl_num)
+                loop_val = True
+            else: print('Invalid input. Try again')
+
+
+    else:
+        sm=Ticket(f_name, l_name, rank, date, unit, act_type, None)
+    print(sm.tl_num)
+    insert_ticket(sm)
+
+def get_delete_inputs():
+    #Gets inputs for the user and then sends those inputs to the delete function
+    print("!WARNING! THIS WILL DELETE TICKET BASED ON INFO GIVEN")
+    time.sleep(3)
+    f_name = input('Please enter First Name: ')
+    l_name = input('Please enter Last Name: ')
+
+    rank = input('Please enter rank, 3 letter format only: ')
+    while len(rank) != 3:  # verify length. 3 letter format only
+        rank = input('Incorrect format. Please enter rank, 3 letter format only: ')
+
+    date = input('Please enter date (YYYY-MM-DD): ')
+    if not date_validator(date):
+        date = input('Incorrect format. Please enter date (YYYY-MM-DD): ')
+        date_validator(date)
+
+    unit = input('Please enter unit: ')
+
+    act_type = action_validator()
+
+    delete_ticket(f_name, l_name, rank, date, unit, act_type)
+
+
 
 
 
